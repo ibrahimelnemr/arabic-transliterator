039a77daa04a6e6ba7d7a9f3e75e00012981df4f
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-01-17 14:33:14 +0200

    Create TransliteratorController.cs

---
+++ backend_dotnet_core/ArabicTransliteratorApi/ArabicTransliteratorApi/Controllers/TransliteratorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArabicTransliteratorApi;
+
+[Route("[controller]")]
+[ApiController]
+public class TransliteratorController : ControllerBase
+{
+
+    [HttpGet]
+    [Route("/")]
+    public string Index() {
+        return "Hello world";
+    }
+
+}
