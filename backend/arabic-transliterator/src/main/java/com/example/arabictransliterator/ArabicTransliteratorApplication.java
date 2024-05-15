ec4f0f8c9f7731f08b90d3b22a70ef6cf22303a7
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-05-16 02:07:25 +0300

    add java backend and react frontend

---
+++ backend/arabic-transliterator/src/main/java/com/example/arabictransliterator/ArabicTransliteratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.arabictransliterator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArabicTransliteratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArabicTransliteratorApplication.class, args);
+	}
+
+}
---
+++ backend/arabic-transliterator/src/test/java/com/example/arabictransliterator/ArabicTransliteratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arabictransliterator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArabicTransliteratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
