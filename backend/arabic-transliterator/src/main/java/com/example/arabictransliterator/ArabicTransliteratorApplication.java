e3a4ae3bdf754e6c818ecb0528cfefb8cc74f47a
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-05-30 20:51:04 +0300

    remove unnecessary code from main function

--- backend/arabic-transliterator/src/main/java/com/example/arabictransliterator/ArabicTransliteratorApplication.java
+++ backend/arabic-transliterator/src/main/java/com/example/arabictransliterator/ArabicTransliteratorApplication.java
@@ -10,27 +10,5 @@ import java.nio.file.Paths;
 public class ArabicTransliteratorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ArabicTransliteratorApplication.class, args);
-		
-
-        String defaultArabicText = "اهلا وسهلا يا غالي";
-        String arabicTextWithDiacritics = "أهلاً وسهلاً يا غالي";
-
-        // String consonantDictionaryFileName = "Arabic Transliterator - Consonants.csv";
-        // String diacriticDictionaryFileName = "Arabic Transliterator - Vowels.csv";
-        
-        // String basePath = Paths.get("").toAbsolutePath().getParent().getParent() + File.separator + "files" + File.separator;
-
-        // String consonantDictionaryPath = Transliterator.checkDictionaryFile(basePath + consonantDictionaryFileName);
-        // String diacriticDictionaryPath = Transliterator.checkDictionaryFile(basePath + diacriticDictionaryFileName);
-        
-        Transliterator transliterator = new Transliterator();
-
-        System.out.println("Default arabic text: " + defaultArabicText);
-
-        System.out.println("Arabic text, consonants only: " + arabicTextWithDiacritics);
-        System.out.println("Text with consonant only transliteration: " + transliterator.transliterate(arabicTextWithDiacritics, true));
-
-        System.out.println("Text with diacritics transliterated: " + transliterator.transliterate(arabicTextWithDiacritics));
 	}
-
 }
