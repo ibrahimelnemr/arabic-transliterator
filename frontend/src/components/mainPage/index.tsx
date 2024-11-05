59fc4e3f82ab86826d551388be540786b14e7d8f
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-11-06 01:30:51 +0200

    Update index.tsx

--- frontend/src/components/mainPage/index.tsx
+++ frontend/src/components/mainPage/index.tsx
@@ -5,26 +5,27 @@ import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import BaseLayout from "../../layouts/baseLayout";
 
 export default function MainPage() {
-  const [arabicText, setArabicText] = useState<String>("");
+  const [arabicText, setArabicText] = useState<string>("");
+  const [transliteratedText, setTransliteratedText] = useState<string>("");
   const title = "Arabic Transliterator";
 
-  useEffect(() => {
-    getTransliteratedSentence();
-  });
+  useEffect(() => {});
 
-  const getTransliteratedSentence = async () => {
+  const getTransliteratedSentence = async (arabicText: string) => {
     try {
       const response = await axios({
         method: "post",
         url: "https://arabize-backend-latest.onrender.com/",
         data: {
-          message: "request sent",
+          arabicText: arabicText,
         },
       });
+
+      console.log(`getTransliteration - response`);
       console.log(response);
 
-      if (response) {
-        setArabicText(response.data);
+      if (response && response.data) {
+        // setTransliteratedText(response.data);
       }
     } catch (err) {
       console.log(err);
@@ -34,14 +35,26 @@ export default function MainPage() {
     <BaseLayout>
       <Row className="text-center m-5 h2 fw-light">{title}</Row>
       <Row className="text-center m-5">
-        <div>{JSON.stringify(arabicText)}</div>
+        <div>
+          {transliteratedText ? transliteratedText : "No transliteration yet"}
+        </div>
       </Row>
       <Row className="text-center m-5">
         <Form>
           <Form.Group>
-            <Form.Control type="text" placeholder="Enter text" />
+            <Form.Control
+              type="text"
+              placeholder="Enter text"
+              value={arabicText}
+              onChange={(e) => {
+                setArabicText(e.target.value);
+              }}
+            />
           </Form.Group>
-          <Button variant="primary" onClick={() => {}}>
+          <Button
+            variant="primary"
+            onClick={() => getTransliteratedSentence(arabicText)}
+          >
             Submit
           </Button>
         </Form>
