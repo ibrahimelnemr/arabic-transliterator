578b7c2449656889cb7884510df92722b4d407c7
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-11-14 00:23:20 +0200

    Update index.tsx

--- frontend/src/layouts/baseLayout/index.tsx
+++ frontend/src/layouts/baseLayout/index.tsx
@@ -8,12 +8,12 @@ type BaseLayoutProps = {
 
 export default function BaseLayout({ children }: BaseLayoutProps) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow flex flex-col p-0 bg-gray-100">
         {children}
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
